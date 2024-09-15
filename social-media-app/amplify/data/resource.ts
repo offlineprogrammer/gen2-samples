272f2d4152ca74b72a351b5e62032d1645f38695
offlineprogrammer <mmalaka@gmail.com>
2024-09-14 20:21:08 -0700

    first commit

---
+++ social-media-app/amplify/data/resource.ts
@@ -0,0 +1,47 @@
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+
+const schema = a.schema({
+  User: a.model({
+    name: a.string().required(),
+    email: a.string().required(),
+    posts: a.hasMany('Post', 'userId'),
+    comments: a.hasMany('Comment', 'userId'),
+  })
+  .authorization((allow) => [allow.owner()]),
+  Post: a.model({
+    userId: a.id(),
+    content: a.string().required(),
+    author: a.belongsTo('User', 'userId'),
+    likes: a.integer(),
+    comments: a.hasMany('Comment', 'postId')
+  })
+  .authorization(allow => [
+    // Allow public to read posts
+    allow.publicApiKey().to(['read']),
+    // Allow owner to create, update, delete posts
+    allow.owner(),
+  ]),
+  Comment: a.model({
+    userId: a.id(),
+    postId: a.id(),
+    content: a.string().required(),
+    author: a.belongsTo('User', 'userId'),
+    post: a.belongsTo('Post', 'postId')
+  }).authorization(allow => [
+    // Allow public to read posts
+    allow.publicApiKey().to(['read']),
+    // Allow owner to create, update, delete posts
+    allow.owner(),
+  ]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'userPool',
+  },
+});
+  
+  
