819f1ba6eaa3fa3328475f88b7d630303ace2f22
offlineprogrammer <mmalaka@gmail.com>
2024-09-16 09:23:33 -0700

    music-streaming-app

---
+++ music-streaming-app/amplify/data/resource.ts
@@ -0,0 +1,85 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  Artist: a
+    .model({
+      name: a.string().required(),
+      bio: a.string(),
+      albums: a.hasMany("Album", "artistId"),
+    })
+    .authorization((allow) => [
+      // Allow public to read instructors
+      allow.publicApiKey().to(["read"]),
+      // Allow owner to create, update, delete Instructors
+      allow.owner(),
+    ]),
+  Album: a
+    .model({
+      title: a.string().required(),
+      releaseDate: a.date().required(),
+      artistId: a.id().required(),
+      artist: a.belongsTo("Artist", "artistId"),
+      tracks: a.hasMany("Track", "albumId"),
+    })
+    .authorization((allow) => [
+      // Allow public to read instructors
+      allow.publicApiKey().to(["read"]),
+      // Allow owner to create, update, delete Instructors
+      allow.owner(),
+    ]),
+  Track: a
+    .model({
+      title: a.string().required(),
+      duration: a.integer().required(),
+      albumId: a.id().required(),
+      album: a.belongsTo("Album", "albumId"),
+      playlists: a.hasMany("PlaylistTrack", "trackId"),
+    })
+    .authorization((allow) => [
+      // Allow public to read instructors
+      allow.publicApiKey().to(["read"]),
+      // Allow owner to create, update, delete Instructors
+      allow.owner(),
+    ]),
+  User: a
+    .model({
+      username: a.string().required(),
+      playlists: a.hasMany("Playlist", "userId"),
+    })
+    .authorization((allow) => [allow.owner()]),
+  Playlist: a
+    .model({
+      name: a.string().required(),
+      userId: a.id().required(),
+      user: a.belongsTo("User", "userId"),
+      tracks: a.hasMany("PlaylistTrack", "playlistId"),
+    })
+    .authorization((allow) => [
+      // Allow public to read instructors
+      allow.publicApiKey().to(["read"]),
+      // Allow owner to create, update, delete Instructors
+      allow.owner(),
+    ]),
+  PlaylistTrack: a
+    .model({
+      playlistId: a.id().required(),
+      trackId: a.id().required(),
+      playlist: a.belongsTo("Playlist", "playlistId"),
+      track: a.belongsTo("Track", "trackId"),
+    })
+    .authorization((allow) => [
+      // Allow public to read instructors
+      allow.publicApiKey().to(["read"]),
+      // Allow owner to create, update, delete Instructors
+      allow.owner(),
+    ]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "userPool",
+  },
+});
