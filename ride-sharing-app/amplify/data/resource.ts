9bc51188f2c455c327b1b42821ecaa314481ca2d
offlineprogrammer <mmalaka@gmail.com>
2024-09-18 11:46:42 -0700

    ride-sharing-app

---
+++ ride-sharing-app/amplify/data/resource.ts
@@ -0,0 +1,60 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  User: a
+    .model({
+      name: a.string().required(),
+      email: a.string().required(),
+      phone: a.string(),
+      role: a.enum(["driver", "passenger"]),
+      rides: a.hasMany("RideUser", "userId"),
+    })
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner(),
+    ]),
+
+  RideUser: a
+    .model({
+      userId: a.id().required(),
+      rideId: a.id().required(),
+      user: a.belongsTo("User", "userId"),
+      ride: a.belongsTo("Ride", "rideId"),
+    })
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner(),
+    ]),
+  Ride: a
+    .model({
+      startLocation: a.string().required(),
+      endLocation: a.string().required(),
+      startTime: a.date().required(),
+      endTime: a.date(),
+      status: a.enum([
+        "requested",
+        "accepted",
+        "in_progress",
+        "completed",
+        "cancelled",
+      ]),
+      fare: a.float(),
+      driverId: a.id(),
+      driver: a.belongsTo("RideUser", "driverId"),
+      passengerId: a.id(),
+      passenger: a.belongsTo("RideUser", "passengerId"),
+    })
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner(),
+    ]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "userPool",
+  },
+});
