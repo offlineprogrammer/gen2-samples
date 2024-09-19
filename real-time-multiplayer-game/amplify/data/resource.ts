ad375c61268c7e4610da1f8cd1f47ab612ea177c
offlineprogrammer <mmalaka@gmail.com>
2024-09-18 19:28:02 -0700

    real-time-multiplayer-game

---
+++ real-time-multiplayer-game/amplify/data/resource.ts
@@ -0,0 +1,64 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  User: a
+    .model({
+      username: a.string().required(),
+      email: a.string().required(),
+      games: a.hasMany("GamePlayer", "playerId"),
+    })
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner(),
+    ]),
+  Game: a
+    .model({
+      name: a.string().required(),
+      status: a.enum(["waiting", "in_progress", "finished"]),
+      players: a.hasMany("GamePlayer", "gameId"),
+      currentTurnId: a.id(),
+      currentTurn: a.belongsTo("User", "c"),
+      winnerId: a.id(),
+      winner: a.belongsTo("User", "winnerId"),
+    })
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner(),
+    ]),
+
+  GamePlayer: a
+    .model({
+      gameId: a.id().required(),
+      game: a.belongsTo("Game", "gameId"),
+      playerId: a.id().required(),
+      player: a.belongsTo("User", "playerId"),
+      score: a.float(),
+    })
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner(),
+    ]),
+
+  Move: a
+    .model({
+      gameId: a.id().required(),
+      game: a.belongsTo("Game", "gameId"),
+      playerId: a.id().required(),
+      player: a.belongsTo("User", "playerId"),
+      position: a.string().required(),
+      timestamp: a.date().required(),
+    })
+    .authorization((allow) => [
+      allow.publicApiKey().to(["read"]),
+      allow.owner(),
+    ]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "userPool",
+  },
+});
