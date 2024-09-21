1d2d35f86ae122d27ed73ed4cc723756bc5a4238
offlineprogrammer <mmalaka@gmail.com>
2024-09-21 15:28:19 -0700

    fitness-tracking-app

--- fitness-tracking-app/amplify/auth/resource.ts
+++ fitness-tracking-app/amplify/auth/resource.ts
@@ -1,11 +1,32 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      },
+      signInWithApple: {
+        clientId: secret('SIWA_CLIENT_ID'),
+        keyId: secret('SIWA_KEY_ID'),
+        privateKey: secret('SIWA_PRIVATE_KEY'),
+        teamId: secret('SIWA_TEAM_ID')
+      },
+      loginWithAmazon: {
+        clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
+        clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET')
+      },
+      facebook: {
+        clientId: secret('FACEBOOK_CLIENT_ID'),
+        clientSecret: secret('FACEBOOK_CLIENT_SECRET')
+      },
+      callbackUrls: [
+        "http://localhost:3000/auth",
+        "https://yourdomain.com/auth",
+      ],
+      logoutUrls: ["http://localhost:3000/", "https://yourdomain.com/"],
+    },
   },
 });
