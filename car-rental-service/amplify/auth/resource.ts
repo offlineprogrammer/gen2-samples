7f59e7b1b9d7856a3103990593cd72bcfdd56c68
offlineprogrammer <mmalaka@gmail.com>
2024-09-21 14:01:44 -0700

    Auth: car-rental-service

--- car-rental-service/amplify/auth/resource.ts
+++ car-rental-service/amplify/auth/resource.ts
@@ -1,11 +1,19 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret("GOOGLE_CLIENT_ID"),
+        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+      },
+
+      callbackUrls: [
+        "http://localhost:3000/auth",
+        "https://yourdomain.com/auth",
+      ],
+      logoutUrls: ["http://localhost:3000/", "https://yourdomain.com/"],
+    },
   },
 });
