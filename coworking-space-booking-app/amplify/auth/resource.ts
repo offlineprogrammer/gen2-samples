70d24c0dc6ffb99ca4658a90c77fe1628352ec2e
offlineprogrammer <mmalaka@gmail.com>
2024-09-21 14:57:42 -0700

    coworking-space-booking-app

--- coworking-space-booking-app/amplify/auth/resource.ts
+++ coworking-space-booking-app/amplify/auth/resource.ts
@@ -1,11 +1,21 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      signInWithApple: {
+        clientId: secret("SIWA_CLIENT_ID"),
+        keyId: secret("SIWA_KEY_ID"),
+        privateKey: secret("SIWA_PRIVATE_KEY"),
+        teamId: secret("SIWA_TEAM_ID"),
+      },
+
+      callbackUrls: [
+        "http://localhost:3000/auth",
+        "https://yourdomain.com/auth",
+      ],
+      logoutUrls: ["http://localhost:3000/", "https://yourdomain.com/"],
+    },
   },
 });
