23e265810a24cf9960f949c4f5c740664ac5240e
offlineprogrammer <mmalaka@gmail.com>
2024-09-21 15:06:39 -0700

    Auth: e-commerce-app

--- e-commerce-app/amplify/auth/resource.ts
+++ e-commerce-app/amplify/auth/resource.ts
@@ -1,11 +1,18 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      facebook: {
+        clientId: secret('FACEBOOK_CLIENT_ID'),
+        clientSecret: secret('FACEBOOK_CLIENT_SECRET')
+      },
+      callbackUrls: [
+        "http://localhost:3000/auth",
+        "https://yourdomain.com/auth",
+      ],
+      logoutUrls: ["http://localhost:3000/", "https://yourdomain.com/"],
+    },
   },
 });
