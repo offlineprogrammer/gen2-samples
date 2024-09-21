7d3ddb0effd191e46375a58c4568c3d21cbc1217
offlineprogrammer <mmalaka@gmail.com>
2024-09-21 15:10:02 -0700

    Auth: event-management-app

--- event-management-app/amplify/auth/resource.ts
+++ event-management-app/amplify/auth/resource.ts
@@ -1,11 +1,18 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      loginWithAmazon: {
+        clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
+        clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET')
+      },
+      callbackUrls: [
+        "http://localhost:3000/auth",
+        "https://yourdomain.com/auth",
+      ],
+      logoutUrls: ["http://localhost:3000/", "https://yourdomain.com/"],
+    },
   },
 });
