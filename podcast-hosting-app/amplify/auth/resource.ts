8717b46770085192b1b392d4de387f358f1c6c9d
offlineprogrammer <mmalaka@gmail.com>
2024-09-22 13:25:34 -0700

    podcast-hosting-app

--- podcast-hosting-app/amplify/auth/resource.ts
+++ podcast-hosting-app/amplify/auth/resource.ts
@@ -1,11 +1,51 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailSubject: "Verify your email for PodcastPro",
+      verificationEmailBody: "Welcome to PodcastPro! Your verification code is {####}",
+    },
+    phone: true,
   },
+  userAttributes: {
+    name: {
+      required: true,
+      mutable: false,
+    },
+   
+    picture: {
+      required: false,
+      mutable: true,
+    },
+
+  },
+ 
+  multifactor: {
+    mode: 'OPTIONAL',
+    sms: true,
+    totp: true,
+  },
+  passwordPolicy: {
+    minLength: 10,
+    requireNumbers: true,
+    requireSpecialCharacters: true,
+    requireUppercase: true,
+    requireLowercase: true,
+  },
+  signUpAttributes: [
+    'email',
+    'name',
+    'picture',
+  ],
+  
+  verificationMechanisms: ['email'],
+  callbackUrls: [
+    'http://localhost:3000/auth',
+    'https://yourpodcastapp.com/auth'
+  ],
+  logoutUrls: [
+    'http://localhost:3000/',
+    'https://yourpodcastapp.com/'
+  ],
 });
