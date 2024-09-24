79b79fd1741ee3ff62723768c17a86e84c0366f3
offlineprogrammer <mmalaka@gmail.com>
2024-09-24 09:25:13 -0700

    recipe-sharing-app

--- recipe-sharing-app/amplify/auth/resource.ts
+++ recipe-sharing-app/amplify/auth/resource.ts
@@ -1,11 +1,34 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      },
+      facebook: {
+        clientId: secret('FACEBOOK_CLIENT_ID'),
+        clientSecret: secret('FACEBOOK_CLIENT_SECRET')
+      },
+      callbackUrls: [
+        'http://localhost:3000/auth',
+        'https://yourrecipeapp.com/auth'
+      ],
+      logoutUrls: [
+        'http://localhost:3000/',
+        'https://yourrecipeapp.com/'
+      ],
+    },
+  },
+  userAttributes: {
+    preferredUsername: {
+      required: true,
+      mutable: false,
+    },
+    profilePicture: {
+      required: false,
+      mutable: true,
+    },
   },
 });
