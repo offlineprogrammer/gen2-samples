aeacc69efbf647e36cc8621d58e3b1563d812694
offlineprogrammer <mmalaka@gmail.com>
2024-10-04 13:10:51 -0700

    recipe sharing app - update

--- recipe-sharing-app/amplify/auth/resource.ts
+++ recipe-sharing-app/amplify/auth/resource.ts
@@ -1,34 +1,38 @@
 import { defineAuth, secret } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
-  loginWith: {
-    externalProviders: {
-      google: {
-        clientId: secret('GOOGLE_CLIENT_ID'),
-        clientSecret: secret('GOOGLE_CLIENT_SECRET')
-      },
-      facebook: {
-        clientId: secret('FACEBOOK_CLIENT_ID'),
-        clientSecret: secret('FACEBOOK_CLIENT_SECRET')
-      },
-      callbackUrls: [
-        'http://localhost:3000/auth',
-        'https://yourrecipeapp.com/auth'
-      ],
-      logoutUrls: [
-        'http://localhost:3000/',
-        'https://yourrecipeapp.com/'
-      ],
-    },
-  },
-  userAttributes: {
-    preferredUsername: {
-      required: true,
-      mutable: false,
-    },
-    profilePicture: {
-      required: false,
-      mutable: true,
-    },
-  },
+	loginWith: {
+		externalProviders: {
+			google: {
+				clientId: secret("GOOGLE_CLIENT_ID"),
+				clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+			},
+			signInWithApple: {
+				clientId: secret("SIWA_CLIENT_ID"),
+				keyId: secret("SIWA_KEY_ID"),
+				privateKey: secret("SIWA_PRIVATE_KEY"),
+				teamId: secret("SIWA_TEAM_ID"),
+			},
+			loginWithAmazon: {
+				clientId: secret("LOGINWITHAMAZON_CLIENT_ID"),
+				clientSecret: secret("LOGINWITHAMAZON_CLIENT_SECRET"),
+			},
+			facebook: {
+				clientId: secret("FACEBOOK_CLIENT_ID"),
+				clientSecret: secret("FACEBOOK_CLIENT_SECRET"),
+			},
+			callbackUrls: ["http://localhost:3000/", "https://yourrecipeapp.com/"],
+			logoutUrls: ["http://localhost:3000/", "https://yourrecipeapp.com/"],
+		},
+	},
+	userAttributes: {
+		preferredUsername: {
+			required: true,
+			mutable: false,
+		},
+		profilePicture: {
+			required: false,
+			mutable: true,
+		},
+	},
 });
--- recipe-sharing-app/amplify/data/resource.ts
+++ recipe-sharing-app/amplify/data/resource.ts
@@ -1,63 +1,57 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
-
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
-
-  User: a.model({
-    name: a.string().required(),
-    recipes: a.hasMany('Recipe', 'authorId'),
-  }).authorization((allow) => [allow.owner()]),
-  Recipe: a.model({
-    title: a.string().required(),
-    ingredients: a.hasMany('Ingredient', 'recipeId'),
-    instructions: a.string().required(),
-    authorId: a.id().required(),
-    author: a.belongsTo('User', 'authorId'),
-    category: a.enum(['appetizer', 'main', 'dessert', 'beverage']),
-    ratings: a.hasMany('Rating', 'recipeId')
-  }).authorization(allow => [
-    // Allow public to read recipes
-    allow.publicApiKey().to(['read']),
-    // Allow owner to create, update, delete recipes
-    allow.owner(),
-  ]),
-  Ingredient: a.model({
-    name: a.string().required(),
-    quantity: a.float().required(),
-    unit: a.string().required(),
-    recipeId: a.id().required(),
-    recipe: a.belongsTo('Recipe', 'recipeId')
-  }).authorization(allow => [
-    // Allow public to read ingredients
-    allow.publicApiKey().to(['read']),
-    // Allow owner to create, update, delete ingredients
-    allow.owner(),
-  ]),
-  Rating: a.model({
-    score: a.integer().required(),
-    userId: a.id().required(),
-    user: a.belongsTo('User', 'userId'),
-    recipeId: a.id().required(),
-    recipe: a.belongsTo('Recipe', 'recipeId')
-  }).authorization(allow => [
-    // Allow public to read the ratings
-    allow.publicApiKey().to(['read']),
-    // Allow owner to create, update, delete the ratings
-    allow.owner(),
-  ]),
-
-
-
- 
+	User: a
+		.model({
+			name: a.string().required(),
+			recipes: a.hasMany("Recipe", "authorId"),
+		})
+		.authorization((allow) => [allow.owner()]),
+	Recipe: a
+		.model({
+			title: a.string().required(),
+			ingredients: a.hasMany("Ingredient", "recipeId"),
+			instructions: a.string().required(),
+			authorId: a.id().required(),
+			author: a.belongsTo("User", "authorId"),
+			category: a.enum(["appetizer", "main", "dessert", "beverage"]),
+			ratings: a.hasMany("Rating", "recipeId"),
+		})
+		.authorization((allow) => [
+			allow.authenticated().to(["read"]),
+			allow.owner(),
+		]),
+	Ingredient: a
+		.model({
+			name: a.string().required(),
+			quantity: a.float().required(),
+			unit: a.string().required(),
+			recipeId: a.id().required(),
+			recipe: a.belongsTo("Recipe", "recipeId"),
+		})
+		.authorization((allow) => [
+			allow.authenticated().to(["read"]),
+			allow.owner(),
+		]),
+	Rating: a
+		.model({
+			score: a.integer().required(),
+			userId: a.id().required(),
+			user: a.belongsTo("User", "userId"),
+			recipeId: a.id().required(),
+			recipe: a.belongsTo("Recipe", "recipeId"),
+		})
+		.authorization((allow) => [
+			allow.authenticated().to(["read"]),
+			allow.owner(),
+		]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
-  },
+	schema,
+	authorizationModes: {
+		defaultAuthorizationMode: "userPool",
+	},
 });
-  
-  
