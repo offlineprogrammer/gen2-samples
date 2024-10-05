8f1055eddc10741244c2e33233aba4f971e6f5d0
offlineprogrammer <mmalaka@gmail.com>
2024-10-05 16:41:16 -0700

    social-media-app update

--- social-media-app/amplify/data/resource.ts
+++ social-media-app/amplify/data/resource.ts
@@ -1,47 +1,45 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  User: a.model({
-    name: a.string().required(),
-    email: a.email().required(),
-    posts: a.hasMany('Post', 'userId'),
-    comments: a.hasMany('Comment', 'userId'),
-  })
-  .authorization((allow) => [allow.owner()]),
-  Post: a.model({
-    userId: a.id(),
-    content: a.string().required(),
-    author: a.belongsTo('User', 'userId'),
-    likes: a.integer(),
-    comments: a.hasMany('Comment', 'postId')
-  })
-  .authorization(allow => [
-    // Allow public to read posts
-    allow.publicApiKey().to(['read']),
-    // Allow owner to create, update, delete posts
-    allow.owner(),
-  ]),
-  Comment: a.model({
-    userId: a.id(),
-    postId: a.id(),
-    content: a.string().required(),
-    author: a.belongsTo('User', 'userId'),
-    post: a.belongsTo('Post', 'postId')
-  }).authorization(allow => [
-    // Allow public to read posts
-    allow.publicApiKey().to(['read']),
-    // Allow owner to create, update, delete posts
-    allow.owner(),
-  ]),
+	User: a
+		.model({
+			name: a.string().required(),
+			email: a.email().required(),
+			posts: a.hasMany("Post", "userId"),
+			comments: a.hasMany("Comment", "userId"),
+		})
+		.authorization((allow) => [allow.owner()]),
+	Post: a
+		.model({
+			userId: a.id(),
+			content: a.string().required(),
+			author: a.belongsTo("User", "userId"),
+			likes: a.integer(),
+			comments: a.hasMany("Comment", "postId"),
+		})
+		.authorization((allow) => [
+			allow.authenticated().to(["read"]),
+			allow.owner(),
+		]),
+	Comment: a
+		.model({
+			userId: a.id(),
+			postId: a.id(),
+			content: a.string().required(),
+			author: a.belongsTo("User", "userId"),
+			post: a.belongsTo("Post", "postId"),
+		})
+		.authorization((allow) => [
+			allow.authenticated().to(["read"]),
+			allow.owner(),
+		]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
-  },
+	schema,
+	authorizationModes: {
+		defaultAuthorizationMode: "userPool",
+	},
 });
-  
-  
