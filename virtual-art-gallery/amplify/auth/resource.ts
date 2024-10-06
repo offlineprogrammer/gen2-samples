7f7bfbffc87f303e31ee9995ad0bcd562dfc0ce3
offlineprogrammer <mmalaka@gmail.com>
2024-10-05 18:18:26 -0700

    virtual art gallery update

--- virtual-art-gallery/amplify/auth/resource.ts
+++ virtual-art-gallery/amplify/auth/resource.ts
@@ -1,58 +1,62 @@
 import { defineAuth, secret } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
-  loginWith: {
-    email: {
-      verificationEmailStyle: "CODE",
-      verificationEmailSubject: "Verify your email for Virtual Art Gallery",
-      verificationEmailBody: (createCode) =>
-        "Welcome to Virtual Art Gallery! Your verification code is: " + createCode(),
-    },
-    phone: {
-      verificationMessage: (createCode) =>
-       "Welcome to Virtual Art Gallery! Your verification code is: " + createCode(),
-    },
-    externalProviders: {
-      google: {
-        clientId: secret("GOOGLE_CLIENT_ID"),
-        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
-      },
-      signInWithApple: {
-        clientId: secret("SIWA_CLIENT_ID"),
-        keyId: secret("SIWA_KEY_ID"),
-        privateKey: secret("SIWA_PRIVATE_KEY"),
-        teamId: secret("SIWA_TEAM_ID"),
-      },
-      facebook: {
-        clientId: secret("FACEBOOK_CLIENT_ID"),
-        clientSecret: secret("FACEBOOK_CLIENT_SECRET"),
-      },
-      callbackUrls: [
-        "http://localhost:3000/",
-        "https://yourvirtualgallery.com/",
-      ],
-      logoutUrls: ["http://localhost:3000/", "https://yourvirtualgallery.com/"],
-    },
-  },
-  userAttributes: {
-    "custom:role": {
-      dataType: "String",
-      maxLen: 20,
-      mutable: false,
-    },
-    profilePicture: {
-      required: false,
-      mutable: true,
-    },
-    "custom:lastVisitDate": {
-      dataType: "DateTime",
-      mutable: true,
-    },
-  },
-  groups: ["Artists", "Visitors", "Administrators"],
+	loginWith: {
+		email: {
+			verificationEmailStyle: "CODE",
+			verificationEmailSubject: "Verify your email for Virtual Art Gallery",
+			verificationEmailBody: (createCode) =>
+				`Welcome to Virtual Art Gallery! Your verification code is: ${createCode()}`,
+		},
+		phone: {
+			verificationMessage: (createCode) =>
+				`Welcome to Virtual Art Gallery! Your verification code is: ${createCode()}`,
+		},
+		externalProviders: {
+			google: {
+				clientId: secret("GOOGLE_CLIENT_ID"),
+				clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+			},
+			signInWithApple: {
+				clientId: secret("SIWA_CLIENT_ID"),
+				keyId: secret("SIWA_KEY_ID"),
+				privateKey: secret("SIWA_PRIVATE_KEY"),
+				teamId: secret("SIWA_TEAM_ID"),
+			},
+			loginWithAmazon: {
+				clientId: secret("LOGINWITHAMAZON_CLIENT_ID"),
+				clientSecret: secret("LOGINWITHAMAZON_CLIENT_SECRET"),
+			},
+			facebook: {
+				clientId: secret("FACEBOOK_CLIENT_ID"),
+				clientSecret: secret("FACEBOOK_CLIENT_SECRET"),
+			},
+			callbackUrls: [
+				"http://localhost:3000/",
+				"https://yourvirtualgallery.com/",
+			],
+			logoutUrls: ["http://localhost:3000/", "https://yourvirtualgallery.com/"],
+		},
+	},
+	userAttributes: {
+		"custom:role": {
+			dataType: "String",
+			maxLen: 20,
+			mutable: false,
+		},
+		profilePicture: {
+			required: false,
+			mutable: true,
+		},
+		"custom:lastVisitDate": {
+			dataType: "DateTime",
+			mutable: true,
+		},
+	},
+	groups: ["Artists", "Visitors", "Administrators"],
 
-  multifactor: {
-    mode: "REQUIRED",
-    sms: true,
-  },
+	multifactor: {
+		mode: "REQUIRED",
+		sms: true,
+	},
 });
--- virtual-art-gallery/amplify/data/resource.ts
+++ virtual-art-gallery/amplify/data/resource.ts
@@ -1,72 +1,72 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  Artist: a
-    .model({
-      name: a.string().required(),
-      bio: a.string(),
-      artworks: a.hasMany("Artwork", "artistId"),
-    })
-    .authorization((allow) => [
-      allow.publicApiKey().to(["read"]),
-      allow.owner(),
-    ]),
-  Artwork: a
-    .model({
-      title: a.string().required(),
-      description: a.string(),
-      imageUrl: a.string().required(),
-      price: a.float(),
-      artistId: a.id().required(),
-      artist: a.belongsTo("Artist", "artistId"),
-      exhibitionsId: a.id(),
-      exhibition: a.belongsTo("Exhibition", "exhibitionsId"),
-      visitors: a.hasMany("VisitorArtwork", "artworkId"),
-    })
-    .authorization((allow) => [
-      allow.publicApiKey().to(["read"]),
-      allow.owner(),
-    ]),
-  Exhibition: a
-    .model({
-      name: a.string().required(),
-      startDate: a.date().required(),
-      endDate: a.date().required(),
-      description: a.string(),
-      artworks: a.hasMany("Artwork", "exhibitionsId"),
-    })
-    .authorization((allow) => [
-      allow.publicApiKey().to(["read"]),
-      allow.owner(),
-    ]),
-  VisitorArtwork: a
-    .model({
-      visitorId: a.id().required(),
-      artworkId: a.id().required(),
-      visitor: a.belongsTo("Visitor", "visitorId"),
-      artwork: a.belongsTo("Artwork", "artworkId"),
-    })
-    .authorization((allow) => [
-      allow.publicApiKey().to(["read"]),
-      allow.owner(),
-    ]),
-  Visitor: a
-    .model({
-      name: a.string().required(),
-      email: a.email().required(),
-      favorites: a.hasMany("VisitorArtwork", "visitorId"),
-    })
-    .authorization((allow) => [
-      allow.group('Admin').to(["read"]),
-      allow.owner(),
-    ]),
+	Artist: a
+		.model({
+			name: a.string().required(),
+			bio: a.string(),
+			artworks: a.hasMany("Artwork", "artistId"),
+		})
+		.authorization((allow) => [
+			allow.authenticated().to(["read"]),
+			allow.owner(),
+		]),
+	Artwork: a
+		.model({
+			title: a.string().required(),
+			description: a.string(),
+			imageUrl: a.string().required(),
+			price: a.float(),
+			artistId: a.id().required(),
+			artist: a.belongsTo("Artist", "artistId"),
+			exhibitionsId: a.id(),
+			exhibition: a.belongsTo("Exhibition", "exhibitionsId"),
+			visitors: a.hasMany("VisitorArtwork", "artworkId"),
+		})
+		.authorization((allow) => [
+			allow.authenticated().to(["read"]),
+			allow.owner(),
+		]),
+	Exhibition: a
+		.model({
+			name: a.string().required(),
+			startDate: a.date().required(),
+			endDate: a.date().required(),
+			description: a.string(),
+			artworks: a.hasMany("Artwork", "exhibitionsId"),
+		})
+		.authorization((allow) => [
+			allow.authenticated().to(["read"]),
+			allow.owner(),
+		]),
+	VisitorArtwork: a
+		.model({
+			visitorId: a.id().required(),
+			artworkId: a.id().required(),
+			visitor: a.belongsTo("Visitor", "visitorId"),
+			artwork: a.belongsTo("Artwork", "artworkId"),
+		})
+		.authorization((allow) => [
+			allow.authenticated().to(["read"]),
+			allow.owner(),
+		]),
+	Visitor: a
+		.model({
+			name: a.string().required(),
+			email: a.email().required(),
+			favorites: a.hasMany("VisitorArtwork", "visitorId"),
+		})
+		.authorization((allow) => [
+			allow.group("Admin").to(["read"]),
+			allow.owner(),
+		]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "userPool",
-  },
+	schema,
+	authorizationModes: {
+		defaultAuthorizationMode: "userPool",
+	},
 });
